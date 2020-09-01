d168abd86894e727f828e93f894eb0ca31ecc984
V10 <victor.fernandez1006@gmail.com>
2020-09-01 00:49:46 -0500

    Segundo Commit - Seguridad con datos (H2) incrustados.

--- src/main/java/com/example/SpringSecurity.java
+++ src/main/java/com/example/SpringSecurity.java
@@ -21,20 +21,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser(
-                        User.withUsername("user")
-                                .password("pass")
-                                .roles("USER")
-                )
-                .withUser(
 
-                        User.withUsername("admin")
-                                .password("pass")
-                                .roles("ADMIN")
-
-
-                )
         ;
     }
 
