ca3a61d03355c61a2bb0635c44d97048705383cf
V10 <victor.fernandez1006@gmail.com>
2020-09-01 00:31:03 -0500

    Primer Commit - Seguridad con datos (H2) incrustados.

---
+++ src/main/java/com/example/SpringSecurityJdbc01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbc01Application.class, args);
+	}
+
+}
